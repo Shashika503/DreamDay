c2a93857a9920fe6ec132f349357294c555595d6
DulanyaSevindi <kahatapitiyagsj@gmail.com>
2025-05-07 07:01:55 +0530

    Add create weddings

--- ViewModels/WeddingCreateViewModel.cs
+++ ViewModels/WeddingCreateViewModel.cs
@@ -1,20 +1,26 @@
-using DreamDay.Models;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using DreamDay.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DreamDay.ViewModels;
 
 public class WeddingCreateViewModel
 {
     [Required]
     public string Title { get; set; }
 
-   [Required]
-[DataType(DataType.Date)]
-public DateTime? Date { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime? Date { get; set; }
 
     [Required]
     public string PlannerId { get; set; }
 
+    [Required(ErrorMessage = "Please select at least one vendor.")]
     public List<int> SelectedVendorIds { get; set; } = new();
 
-    public List<ApplicationUser> AvailablePlanners { get; set; }
-    public List<Vendor> AvailableVendors { get; set; }
+    public List<ApplicationUser>? AvailablePlanners { get; set; }
+    public List<Vendor>? AvailableVendors { get; set; }
 }
